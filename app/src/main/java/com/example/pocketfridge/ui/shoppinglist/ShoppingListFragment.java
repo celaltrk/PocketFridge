c37102e066bd8be75b734063ff5b20cf12624b40
celaltrk <celalsalih64@gmail.com>
2022-05-03 10:34:02 +0300

    Create SL
    Small code added

--- app/src/main/java/com/example/pocketfridge/ui/shoppinglist/ShoppingListFragment.java
+++ app/src/main/java/com/example/pocketfridge/ui/shoppinglist/ShoppingListFragment.java
@@ -11,14 +11,23 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.pocketfridge.R;
+import com.example.pocketfridge.adapter.ItemAdapter;
+import com.example.pocketfridge.data.DBHelper;
 import com.example.pocketfridge.databinding.FragmentShoppinglistBinding;
+import com.example.pocketfridge.fridgeItems.Product;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class ShoppingListFragment extends Fragment {
 
     private ShoppingListViewModel shoppingListViewModel;
     private FragmentShoppinglistBinding binding;
-
+    RecyclerView fridgeRecyclerView;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         shoppingListViewModel =
@@ -33,5 +42,22 @@ public class ShoppingListFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        createSL();
+    }
+
+    public void createSL() {
+        ArrayList<Product> products = new ArrayList<>();
+        products.add(new Product("sample","a","a1",null));
+        Collections.sort(products);
+        fridgeRecyclerView = (RecyclerView) getView().findViewById((R.id.SL_recyclerView));
+        ItemAdapter adapter = new ItemAdapter(products);
+        fridgeRecyclerView.setHasFixedSize(true);
+        fridgeRecyclerView.setLayoutManager(new LinearLayoutManager(this.getActivity()));
+        fridgeRecyclerView.setAdapter(adapter);
     }
 }
